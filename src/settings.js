600dd688468d2f314975cff65931a1c382d4762b
Ben Jack <ben@Staff-WG402-Ben.local>
2019-04-10 18:32:41 +1200

    settings adjusted

--- src/settings.js
+++ src/settings.js
@@ -4,18 +4,18 @@ export const render_texture_size = {x:1920, y:1080};
 
 //the particle position and velocity textures will be a square of this size.
 //there will be a total of particle_count_sq^2 particles
-export const particle_count_sq   = 80;
+export const particle_count_sq   = 100;
 
 
 //Particles
 export const particle_size       = 17;
-export const particle_speed      = 0.0015;
+export const particle_speed      = 0.001;
 export const sniff_size          = 17.0/1920.0;
-export const sniff_odds          = 0.6;
+export const sniff_odds          = 0.1;
 export const sniff_samples_sq    = 5;
 
 
 //Rendering
-export const fade_amount = 0.98;
+export const fade_amount = 0.96;
 export const lower_mask_bound = 0.7;
 export const upper_mask_bound = 1.0;
