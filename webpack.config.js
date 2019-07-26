029c0bb2c12459393c9afef5c3b819fc6fd406eb
Ben Jack <benthejack@gmail.com>
2019-07-26 17:12:55 +0100

    updated js output file name

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: [path.join(__dirname, "src/index.js")],
   output: {
     path: path.join(__dirname, 'www/', 'scripts/'),
-    filename: "hello_field.bundle.js"
+    filename: "hello_webgl.bundle.js"
   },
 
   module: {
